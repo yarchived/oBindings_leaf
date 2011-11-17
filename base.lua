0343e0363f8f8bfa0523a04db45e8e7fb34e7219
yaroot <yaroot@gmail.com>
2011-11-17 16:39:10 +0800

    generate open chat macro

--- base.lua
+++ base.lua
@@ -2,6 +2,10 @@
 local _NAME, _NS = ...
 _NS.CLASS = select(2, UnitClass'player')
 
+local open_chat = function(key)
+    return ('m|/run ChatFrame_OpenChat"/%s" '):format(key)
+end
+
 local base = {
     -- base movement
     W = 'MOVEFORWARD',
@@ -12,29 +16,29 @@ local base = {
     F6 = [[m|/run local n,r=UnitName'target';n=r and(n..'-'..r)or n;return n and ChatFrame_OpenChat('/w '..n..' ')]],
     F7 = 'm|/inspect',
     F8 = 'FOCUSTARGET',
-    F9 = "m|/run ChatFrame_OpenChat'/g '",
-    F10 = "m|/run ChatFrame_OpenChat'/5 '",
-    F11 = "m|/run ChatFrame_OpenChat'/s '",
+    F9 = open_chat'g',
+    F10 = open_chat'5',
+    F11 = open_chat's'
     F12 = [[m|/run local i,t,c=IsInInstance();c=(i and t=='pvp')and'/bg 'or(GetRealNumRaidMembers()>0)and'/ra 'and(GetRealNumPartyMembers()>0)and'/p 'or'/s ';ChatFrame_OpenChat(c)]],
 
     alt = {
         V = 'NAMEPLATES',
-        F10 = "m|/run ChatFrame_OpenChat'/9 '",
-        --F11 = "m|/run ChatFrame_OpenChat'/rw '",
-        F12 = "m|/run ChatFrame_OpenChat'/bg '",
+        F10 = open_chat'9',
+        --F11 = open_chat'rw',
+        F12 = open_chat'bg',
     },
 
     ctrl = {
-        F9 = "m|/run ChatFrame_OpenChat'/o '",
-        F10 = "m|/run ChatFrame_OpenChat'/6 '",
-        F11 = "m|/run ChatFrame_OpenChat'/rw '",
-        F12 = "m|/run ChatFrame_OpenChat'/ra '",
+        F9  = open_chat'o',
+        F10 = open_chat'6',
+        F11 = open_chat'rw',
+        F12 = open_chat'ra',
     },
 
     shift = {
-        F10 = "m|/run ChatFrame_OpenChat'/8 '",
-        F11 = "m|/run ChatFrame_OpenChat'/y '",
-        F12 = "m|/run ChatFrame_OpenChat'/p '",
+        F10 = open_chat'8',
+        F11 = open_chat'y',
+        F12 = open_chat'p',
     },
 }
 
