6b407a87840c99e6c4f917a346bca280eec610c0
yaroot <yaroot@gmail.com>
2011-11-17 10:43:57 +0800

    .

--- leaf.lua
+++ leaf.lua
@@ -2,7 +2,7 @@
 local _NAME, _NS = ...
 local oBindings = _G[_NAME]
 
-oBindings:RegisterKeyBindings(_NAME, _NS.BASE, _NS[_NS.CLASS])
+oBindings:RegisterKeyBindings(_NAME, _NS.BASE, _NS[_NS.CLASS] or {})
 
 oBindings:UnregisterEvent'ACTIVE_TALENT_GROUP_CHANGED'
 function oBindings:PLAYER_TALENT_UPDATE(event)
