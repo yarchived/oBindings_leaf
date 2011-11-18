5cd7a4be3020e150a81895dc024ca52a0eda58d6
yaroot <yaroot@gmail.com>
2011-11-18 10:20:17 +0800

    add druid stuff

--- druid.lua
+++ druid.lua
@@ -2,11 +2,33 @@
 local _NAME, _NS = ...
 if(_NS.CLASS ~= 'DRUID') then return end
 
+local spells = {
+    Aquatic         = 1066,
+    Flight          = 33943,
+    SwiftFlight     = 40120,
+    Travel          = 783,
+    Bear            = 5487,
+    Cat             = 768,
+    Tree            = 33891,
+    Moonkin         = 62795,
+}
+for k, v in next, spells do
+    spells[k] = GetSpellInfo(v)
+end
+
 _NS.DRUID = {
-    F1 = 'm|/click yDruidFrameBear',
-    F2 = 'm|/click yDruidFrameSmart',
-    F3 = 'm|/click yDruidFrameCat',
-    F4 = 'm|/click yDruidFrameUMOUNT',
-    F5 = 'm|/click yDruidFrameMoonkinOrTree',
+    F1 = 'm|/cast ' ..spells.Bear,
+    --F2 = 'm|/click yDruidFrameSmart',
+    F2 = 'm|/stopmacro [flying,combat]' ..
+        '\n/cast [swimming]!'..spells.Aquatic ..
+        ';[combat]!' ..spells.Travel ..
+        ';[flyable,nocombat]!'.. spells.SwiftFlight ..
+        '\n/stopmacro [combat][mounted][flyable]' ..
+        '\n/cancelform' ..
+        (SlashCmdList.NORU_MOUNT and '\n/noru' or '') ..
+        '\n/cast '..spells.Travel,
+    F3 = 'm|/cast ' ..spells.Cat,
+    F4 = 'm|/dismount\n/cancelform',
+    F5 = 'm|/cast '..spells.Moonkin..'\n/cast '..spells.Tree,
 }
 
