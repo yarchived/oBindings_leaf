56730d94674371e2be290da9d03e0bff666ff08b
yaroot <yaroot@gmail.com>
2012-04-11 20:09:15 +0800

    remove LibMounts

--- mount.lua
+++ mount.lua
@@ -1,19 +1,44 @@
 
 -- mount.lua
--- takes advantage of LibMounts to write a simple random mount addon
--- LibMounts
--- http://www.wowace.com/addons/libmounts-1-0/
+-- simple mount addon
 
-local LibMounts = LibStub 'LibMounts-1.0'
 
-local num_comps = 0
-local spellid2compid = {}
-local update_comp = function()
-    num_comps = GetNumCompanions'MOUNT'
-    wipe(spellid2compid)
-    for i = 1, num_comps do
-        local _, _, id = GetCompanionInfo('MOUNT', i)
-        spellid2compid[id] = i
+local band = bit.band
+local tinsert = table.insert
+
+local FLAG_GROUND = 0x1               -- Ground 
+local FLAG_FLY = 0x2               -- Can fly 
+local FLAG_FLOAT = 0x4               -- Floats above ground 
+local FLAG_UNDERWATER = 0x8               -- Underwater 
+--local CANJUMP = 0x10              -- Can jump (turtles cannot)
+
+local SEA_LEGS = GetSpellInfo(73701)
+
+local special = {
+    VASHJIR = {
+        [75207] = true, --Abyssal Seahorse
+    },
+    AHNQIRAJ = {
+        [26054] = true,
+        [25953] = true,
+        [26056] = true,
+        [26055] = true,
+    }
+}
+
+local get_current_mount_type = function()
+    if(IsIndoors()) then
+        return
+    elseif(UnitAura('player', SEA_LEGS)) then
+        return 'VASHJIR'
+    elseif(IsSwimming()) then
+        return 'WATER'
+    elseif(IsFlyableArea() and IsUsableSpell(60025)) then
+        return 'AIR'
+    elseif(IsUsableSpell(26054)) then
+        return 'AHNQIRAJ'
+    elseif(IsUsableSpell(43688)) then
+        return 'GROUND'
     end
 end
 
@@ -24,9 +49,50 @@ local is_empty = function(list)
     return true
 end
 
+local in_the_list = function(item, list)
+    if(item and list) then
+        if(list[item]) then
+            return true
+        end
+    else
+        return true
+    end
+end
+
+local bit_and = function(a, b)
+    if(a and b) then
+        return band(a, b) ~= 0
+    end
+    return true
+end
+
 local _mount_list = {}
 local get_mount_list = function(state)
-    local list = LibMounts:GetMountList(state, wipe(_mount_list))
+    local list = wipe(_mount_list)
+    local flag, special_lst
+    if(state == 'VASHJIR') then
+        -- flag = FLAG_UNDERWATER
+        special_lst = special.VASHJIR
+    elseif(state == 'AHNQIRAJ') then
+        -- flag = FLAG_GROUND
+        special_lst = special.AHNQIRAJ
+    elseif(state == 'AIR') then
+        flag = FLAG_FLY
+    elseif(state == 'GROUND') then
+        flag = FLAG_GROUND
+    --elseif(state == WATER) then
+    end
+
+    if not(flag or special_lst) then return end
+
+    for i = 1, GetNumCompanions'MOUNT' do
+        local creatureID, creatureName, spellid, icon, issummoned,
+            mountType = GetCompanionInfo('MOUNT', i)
+        if(bit_and(mountType, flag) and in_the_list(spellid, special_lst)) then
+            tinsert(list, i)
+        end
+    end
+
     if(not is_empty(list)) then
         return list
     end
@@ -37,24 +103,14 @@ SlashCmdList.LEAF_MOUNT = function()
     if(InCombatLockdown()) then return end
     if(IsMounted()) then return Dismount() end
 
-    if(GetNumCompanions'MOUNT' ~= num_comps) then
-        update_comp()
-    end
+    local state = get_current_mount_type()
+    if(state) then
+        local list = get_mount_list(state)
 
-    local statePrimary, stateSecondary, stateTertiary = LibMounts:GetCurrentMountType()
-    local mntlist = get_mount_list(statePrimary) 
-        or get_mount_list(stateSecondary)
-        or get_mount_list(stateTertiary)
-
-    if(not mntlist) then return end
-    local list = wipe(filtered_list)
-    for id in next, mntlist do
-        if(spellid2compid[id]) then
-            table.insert(list, id)
+        if(list) then
+            return CallCompanion('MOUNT', list[math.random(#list)])
         end
     end
-    local spellid = list[math.random(#list)]
-    return CallCompanion('MOUNT', spellid2compid[spellid])
 end
 
 SLASH_LEAF_MOUNT1 = '/mount'
